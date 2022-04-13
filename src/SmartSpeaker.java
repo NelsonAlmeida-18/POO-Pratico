2432c4625ae74b14aac938965c6758ac2c2b7f9b
Gustavo Pereira <carlosgsp39@gmail.com>
2022-04-13 16:00:38 +0100

    added methods for smartSpeakers

--- src/SmartSpeaker.java
+++ src/SmartSpeaker.java
@@ -10,40 +10,67 @@ package src;
 public class SmartSpeaker extends SmartDevice {
     public static final int MAX = 20; //volume máximo
     
+    private int id;
     private int volume;
-    private String channel;
+    private String radio; 
+    private Boolean state; // define the speaker state (on/off)
+    private String brand;
 
     /**
      * Constructor for objects of class SmartSpeaker
      */
     public SmartSpeaker() {
-        // initialise instance variables
+        // create smartspeaker - on by default
         this.volume = 10;
+        this.state = true; //on
     }
 
-    public SmartSpeaker(String s) {
-        // initialise instance variables
+    public SmartSpeaker(String station) {
+        // create smartspeaker mentioning radio station makes it on by default
         this.volume = 10;
-        this.channel = s;
+        this.state = true; //on
+        this.radio = station;
     }
 
-    public SmartSpeaker(String cod, String channel, int i) {
-        // initialise instance variables
+    public SmartSpeaker(String brand, String station, int i) {
+        // create smartspeaker mentioning radio station and brand makes it on by default
+        this.id = i;
         this.volume = 10;
+        this.radio = station;
+        this.state = true; //on
+        this.brand = brand;
     }
 
-    public void volumeUp() {
-        if (this.volume<MAX) this.volume++;
-    }
+    //SETTERS
 
-    public void volumeDown() {
-        if (this.volume>0) this.volume--;
-    }
+    public void setStation(String station) {this.radio = station;}
 
-    public int getVolume() {return 0;}
+    public void setBrand(String brand) {this.brand = brand;}
+
+    //GETTERS
+
+    public String getBrand() {return this.brand;}
+
+    public String getStation() {return this.radio;}
+
+    public int getVolume() {return this.volume;}
+
+    public Boolean getState() {return this.state;}
+
+
+    //SMARTSPEAKER METHODS
+
+    public void turnOn() {this.state = true;}
+
+    public void turnOff() {this.state = false;}
+
+    public void volumeUp() {if (this.volume<MAX) this.volume++;}
+
+    public void volumeDown() {if (this.volume>0) this.volume--;}
+
     
-    public String getChannel() {return "";}
+
     
-    public void setChannel(String c) {this.channel = c;}
+
 
 }
