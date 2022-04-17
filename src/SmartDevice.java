cee83a7b51dd52da7d80029077da57ec1c715601
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2022-04-17 22:24:35 +0100

    Updates smartDevices

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -1,5 +1,6 @@
 package src;
 import java.time.LocalDateTime;
+import java.lang.StringBuilder;
 
 /**
  * A classe SmartDevice é um contactor simples.
@@ -34,6 +35,13 @@ public class SmartDevice{
         this.consumo = 0;
     }
 
+    public SmartDevice(SmartDevice sd){
+        this.id = sd.getID();
+        this.on = sd.getOn();
+        this.consumo=sd.getConsumo();
+        this.ultimaVezLigado=sd.getUltimaVezLigado();
+    }
+
     public void turnOn() {
         this.on = true;
     }
@@ -48,9 +56,37 @@ public class SmartDevice{
     
     public String getID() {return this.id;}
 
+    public LocalDateTime getUltimaVezLigado(){return this.ultimaVezLigado;}
+
     public void setConsumo(float consumo){ this.consumo = consumo; }
 
     public float getConsumo(){ return this.consumo; }
 
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Id do dispositivo: ");
+        sb.append(this.id);
+        sb.append("\nConsumo do dispositivo: ");
+        sb.append(this.consumo);
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    public SmartDevice clone(){
+        return new SmartDevice(this);
+    }
+
+
+    //rever
+    public boolean equals(Object obj){
+        if (this==obj)
+            return true;
+        
+        if (obj==null||obj.getClass()!=this.getClass())
+            return false;
+        
+        SmartDevice newSD = (SmartDevice) obj;
+        return (this.id.equals(newSD.getID())  &&  this.on==newSD.getOn()  &&  this.consumo==newSD.getConsumo());
+    }
 
 }
