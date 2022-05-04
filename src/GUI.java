b309cafefc48771f35a2bceaf1a1459e73c9cd6b
cgustavop3 <carlosgsp39@gmail.com>
2022-05-04 21:06:17 +0100

    starting GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,38 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class GUI {  
     
     public GUI(){
-        JFrame frame = new JFrame();
         JPanel panel = new JPanel();
     }
-
+/*
     public static void main(String[] args){
-        
-        new GUI();
-    }
+
+        JButton button1 = new JButton();
+        button1.setText("Criar SmartCity");
+        //button1.setBounds(200,100,100,500);
+        button1.setBounds(10,10,20,10);
+
+        JButton button2 = new JButton();
+        button2.setText("Carregar de um ficheiro objeto");
+        //button1.setBounds(200,100,100,500);
+        button2.setBounds(10,20,20,10);
+
+        JButton button3 = new JButton();
+        button3.setText("Carregar de um ficheiro log");
+        //button1.setBounds(200,100,100,500);
+        button3.setSize(20,10);
+        button2.setBounds(10,50,20,10);
+
+
+        JFrame frame = new JFrame();
+        frame.setTitle("SmartEcosystem");
+        frame.setSize(600,700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        new MenuInicial();
+    }*/
 }
